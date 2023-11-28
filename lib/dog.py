62aa237cabf68864120186db83e77c8fca5fa582
Edith Wanguba <wanguamor@gmail.com>
2023-11-29 01:51:38 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        return f"{self.name} says Woof!"
+    
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        return f"{self.name} says 'Hello World!'"
+
